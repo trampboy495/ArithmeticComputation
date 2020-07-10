9051784b4b5108863ecec5a7f7cd60d0a3d85554
trampboy495 <histroylover@gmail.com>
2020-07-10 12:12:55 +0530

    [siddhesh] add.UC6 which stores computation result in dictionary

--- SortingComputation.sh
+++ SortingComputation.sh
@@ -4,3 +4,5 @@ Computation1=$((a+b*c))
 Computation2=$((a*b+c))
 Computation3=$((c+a/b))
 Computation4=$((a%b+c))
+declare -A ComputationResult
+ComputationResult=([1]=$Computation1 [2]=$Computation2 [3]=$Computation3 [4]=$Computation4)
